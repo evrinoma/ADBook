f90129c063854578b78358f1262f00404a55c71a
nikolns <nikolns@ite-ng.ru>
2019-03-26 10:28:55 +0300

    remove unused logic

--- copy.java
+++
@@ -1,1 +0,0 @@
-(function() { alert(\"call\");console.log(document); var mode = document.designMode; document.designMode = \"on\"; document.execCommand(\"copy\"); document.designMode = mode;alert(\"end call\");})()
